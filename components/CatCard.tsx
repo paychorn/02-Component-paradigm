9ba0fbb18ca9dc93f3bf40db8e825e24373c205b
ggm <86537060+paychorn@users.noreply.github.com>
2021-08-12 13:55:30 +0700

    Update CatCard.tsx

--- components/CatCard.tsx
+++ components/CatCard.tsx
@@ -9,7 +9,7 @@ const CatCard = ({ title, author, url }) => (
     style={{ width: 240, margin: "10px" }}
     cover={<img alt="example" src={url} />}
   >
-    <Meta title={title} description={"By" + author} />
+    <Meta title={title} description={"By "+ author} />
   </Card>
 );
 
